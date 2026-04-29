79f0c27e7fcf9e288b22024afbf6550b6a2877db
SuryakantK27 <kulkarniisuryakant27@gmail.com>
2026-04-29 17:13:30 +0530

    Improved UI

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 import socket
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello from DevOps Project 🚀\nHostname: {socket.gethostname()}"
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname(),
+        time=datetime.datetime.now()
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
